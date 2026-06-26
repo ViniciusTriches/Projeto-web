36b83e670e4a6124fd668e5e21fe3cc5c009136f
ViniciusTriches <viniciusstriches@outlook.com>
2026-06-26 20:15:22 -0300

    fix: configura binding de porta explicito para compatibilidade com Render

--- src/Marketplace.Web.MVC/Program.cs
+++ src/Marketplace.Web.MVC/Program.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddControllersWithViews();
 
 var config = builder.Configuration;
